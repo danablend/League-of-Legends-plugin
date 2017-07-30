f1a2b6bbb27852246d01834edbaef3297caec219
danablend <danablend@gmail.com>
2017-07-30 12:58:29 +0200

    Added champions enum

---
+++ src/com/oasis/lol/champions/ChampionType.java
@@ -0,0 +1,14 @@
+package com.oasis.lol.champions;
+
+/**
+ * Created by Danablend on 30/07/2017 at 12:49.
+ */
+
+public enum ChampionType {
+
+    ASSASSIN,
+    BRUISER,
+    MAGE,
+    TANK,
+
+}
